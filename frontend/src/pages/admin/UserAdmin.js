5a0d64a48621e51410b526540e88688dfb555ec3
aptech-group4 <nhanptts2201006@fpt.edu.vn>
2023-12-11 10:03:24 +0700

    User Detail in DashBoard

--- frontend/src/pages/admin/UserAdmin.js
+++ frontend/src/pages/admin/UserAdmin.js
@@ -1,74 +1,149 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TextField,
+  TableSortLabel,
+} from "@mui/material";
 
 function UserAdmin(props) {
   const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortCriteria, setSortCriteria] = useState({
+    column: "userId",
+    direction: "asc",
+  });
 
+  // Fetch users data
   useEffect(() => {
-    fetch('http://localhost:8080/api/project4/nhan/users/list') 
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
+    fetch("http://localhost:8080/api/project4/nhan/users/list")
+      .then((response) =>
+        response.ok
+          ? response.json()
+          : Promise.reject("Network response was not ok")
+      )
+      .then((data) => {
+        if (Array.isArray(data)) {
+          setUsers(data);
+          setFilteredUsers(data); // Set filteredUsers after users is set
+        } else {
+          throw new Error("Data received is not an array");
         }
-        return response.json();
       })
-      .then(data => {
-        console.log(data)
-        if (!Array.isArray(data)) {
-          throw new Error('Data received is not an array');
-        }
-        setUsers(data);
-      })
-      .catch(error => {
-        console.error('Error fetching users:', error);
-      });
+      .catch((error) => console.error("Error fetching users:", error));
   }, []);
+
+  // Handle search functionality
+  const handleSearch = (event) => {
+    const searchValue = event.target.value.toLowerCase();
+    setSearchTerm(searchValue);
+    const filteredData = users.filter(
+      (user) =>
+        user.name.toLowerCase().includes(searchValue) ||
+        user.email.toLowerCase().includes(searchValue)
+    );
+    setFilteredUsers(filteredData);
+  };
+
+  // Handle sorting functionality
+  const handleSort = (column) => {
+    const isAsc =
+      sortCriteria.column === column && sortCriteria.direction === "asc";
+    setSortCriteria({ column, direction: isAsc ? "desc" : "asc" });
+
+    const sortedData = [...filteredUsers].sort((a, b) => {
+      let comparison = 0;
+
+      if (column === "isActive") {
+        comparison = a.isActive === b.isActive ? 0 : a.isActive ? -1 : 1;
+      } else if (column === "userRoles") {
+        comparison = a.userRoles.join().localeCompare(b.userRoles.join());
+      } else {
+        comparison = a[column].toString().localeCompare(b[column].toString());
+      }
+
+      return (sortCriteria.direction === "asc" ? 1 : -1) * comparison;
+    });
+
+    setFilteredUsers(sortedData);
+  };
+
+  // Render the component
   return (
     <div>
       <h1>User List</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>User ID</th>
-            <th>Email</th>
-            <th>Name</th>
-            <th>Active</th>
-            <th>Avatar</th>
-            <th>User Roles</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.userId}>
-              <td>{user.userId}</td>
-              <td>{user.email}</td>
-              <td>{user.name}</td>
-              <td>{user.isActive ? "Active" : "Inactive"}</td>
-              <td>
-                {user.avatar && (
-                  <img
-                    src={user.avatar}
-                    alt="Avatar"
-                    style={{ width: "50px", height: "50px" }}
-                  />
-                )}
-              </td>
-              <td>
-                <ul>
-                  {user.userRoles.map((role) => (
-                    <li key={role}>{role}</li>
-                  ))}
-                </ul>
-              </td>
-              <td>
-                <button>Delete</button>
-                <Link to={`detail/${user.userId}`}>View Detail</Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <TextField
+        label="Search"
+        variant="outlined"
+        value={searchTerm}
+        onChange={handleSearch}
+        style={{ marginBottom: "20px" }}
+      />
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {/* Sorting for User ID, Email, Name, Active status, User Roles */}
+              {["userId", "email", "name","avatar", "isActive", "userRoles"].map(
+                (header) => (
+                  <TableCell key={header}>
+                    <TableSortLabel
+                      active={sortCriteria.column === header}
+                      direction={
+                        sortCriteria.column === header
+                          ? sortCriteria.direction
+                          : "asc"
+                      }
+                      onClick={() => handleSort(header)}
+                    >
+                      {header.charAt(0).toUpperCase() + header.slice(1)}
+                    </TableSortLabel>
+                  </TableCell>
+                )
+              )}
+              <TableCell>Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredUsers.map((user) => (
+              <TableRow key={user.userId}>
+                {/* Render user data */}
+                <TableCell>{user.userId}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>
+                  {user.avatar && (
+                    <img
+                      src={user.avatar}
+                      alt="Avatar"
+                      style={{ width: "50px", height: "50px" }}
+                    />
+                  )}
+                </TableCell>
+                <TableCell>{user.isActive ? "Inactive" : "Active"}</TableCell>
+                <TableCell>
+                  <ul>
+                    {user.userRoles.map((role, index) => (
+                      <li key={index}>{role}</li>
+                    ))}
+                  </ul>
+                </TableCell>
+                <TableCell>
+                  <button>Delete</button>
+                  <Link to={`detail/${user.userId}`}>View Detail</Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
--- frontend/src/pages/admin/UserAdmin/UserAdminDetail.js
+++ frontend/src/pages/admin/UserAdmin/UserAdminDetail.js
@@ -1,49 +1,162 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import {
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  TextField,
+  Button,
+  CircularProgress,
+  Avatar,
+  Select,
+  MenuItem,
+  InputLabel,
+} from "@mui/material";
 
-
-const  UserAdminDetail = () => {
+const UserAdminDetail = () => {
   const { userId } = useParams();
-  console.log(userId);
-    const [userDetails, setUserDetails] = useState(null);
+  const [userDetails, setUserDetails] = useState(null);
+  const [updatedUserDetails, setUpdatedUserDetails] = useState({});
+  const [avatarFile, setAvatarFile] = useState(null);
 
-    useEffect(() => {
-      // Fetch user details when the component mounts
-      const fetchUserDetails = async () => {
-        try {
-          const response = await axios.get(`http://localhost:8080/api/project4/nhan/users/detail/${userId}`);
-          console.log(response);
-          setUserDetails(response.data);
-        } catch (error) {
-          console.error('Error fetching user details:', error);
+  // Replace this with actual roles fetched from your backend or defined in your system
+  const userRoles = ["Admin", "User", "Moderator", "Guest"];
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/api/project4/nhan/users/detail/${userId}`
+        );
+        setUserDetails(response.data);
+        setUpdatedUserDetails(response.data); // Initialize form with existing data
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
+    };
+
+    fetchUserDetails();
+  }, [userId]);
+
+  const handleInputChange = (e) => {
+    setUpdatedUserDetails({
+      ...updatedUserDetails,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleAvatarChange = (e) => {
+    setAvatarFile(e.target.files[0]);
+  };
+
+  const handleRoleChange = (e) => {
+    setUpdatedUserDetails({
+      ...updatedUserDetails,
+      userRoles: [e.target.value],
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append("avatar", avatarFile);
+      formData.append("userDetails", JSON.stringify(updatedUserDetails));
+
+      await axios.put(
+        `http://localhost:8080/api/project4/nhan/users/update/${userId}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
         }
-      };
-  
-      fetchUserDetails();
-    }, [userId]);
-  
-    return (
-      <div>
-        {userDetails ? (
-          <div>
-            <h1>User Details</h1>
-            <div>
-              <img src={userDetails.avatar} alt="User Avatar" />
-              <p>User Name: {userDetails.userName}</p>
-              <p>Email: {userDetails.email}</p>
-              <p>Date of Birth: {userDetails.dateOfBirth}</p>
-              <p>User Roles: {userDetails.userRoles.join(', ')}</p>
-              <p>Amount of Posts: {userDetails.amountOfPosts}</p>
-              <p>Amount of Gems: {userDetails.amountOfGems}</p>
-              <p>Amount of User Achievements: {userDetails.amountOfUserAchievements}</p>
-            </div>
+      );
+      setUserDetails(updatedUserDetails);
+    } catch (error) {
+      console.error("Error updating user details:", error);
+    }
+  };
+
+  return (
+    <Paper style={{ padding: "20px", maxWidth: "600px", margin: "20px auto" }}>
+      {userDetails ? (
+        <form onSubmit={handleSubmit}>
+          <Typography variant="h4" style={{ marginBottom: "20px" }}>
+            Edit User Details
+          </Typography>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "20px",
+            }}
+          >
+            <Avatar
+              src={userDetails.avatar}
+              style={{ width: "100px", height: "100px", marginRight: "20px" }}
+            />
+            <input type="file" onChange={handleAvatarChange} />
           </div>
-        ) : (
-          <p>Loading user details...</p>
-        )}
-      </div>
-    );
-}
+          <List>
+            <ListItem>
+              <TextField
+                fullWidth
+                label="User Name"
+                name="userName"
+                value={updatedUserDetails.userName}
+                onChange={handleInputChange}
+                margin="normal"
+              />
+            </ListItem>
+            <ListItem>
+              <TextField
+                fullWidth
+                label="Email"
+                name="email"
+                value={updatedUserDetails.email}
+                disabled
+                margin="normal"
+              />
+            </ListItem>
+            <ListItem>
+              <Select
+                labelId="test-select-label"
+                label={"Label1233333"}
+                fullWidth
+                name="User Role"
+                value={
+                  updatedUserDetails.userRoles
+                    ? updatedUserDetails.userRoles[0]
+                    : ""
+                }
+                onChange={handleRoleChange}
+              >
+                {userRoles.map((role) => (
+                  <MenuItem key={role} value={role}>
+                    {role}
+                  </MenuItem>
+                ))}
+              </Select>
+            </ListItem>
+            {/* Other fields can be added similarly */}
+          </List>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            style={{ marginTop: "20px" }}
+          >
+            Update Profile
+          </Button>
+        </form>
+      ) : (
+        <CircularProgress style={{ display: "block", margin: "auto" }} />
+      )}
+    </Paper>
+  );
+};
 
 export default UserAdminDetail;
